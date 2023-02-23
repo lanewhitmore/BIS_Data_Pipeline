4370eb6f958c6b56838900981200dc460a91d22a
Lane Whitmore <lwhitmore@sandiego.edu>
2023-02-22 16:19:45 -0800

    updated .py file, new .bat file for automation
    The .py update includes setting up _data_path os environment and reassigning them to the prior path occurences

--- src/bis_pipe.py
+++ src/bis_pipe.py
@@ -40,6 +40,7 @@ _user = os.environ.get('USER')
 _port = int(os.environ.get('PORT'))
 _passwd = os.environ.get('PASSWORD')
 _db = os.environ.get('DB_NAME')
+_data_path = os.environ.get('DATA_PATH')
 
 # Setup logging
 logging.basicConfig(filename='pipeline.log', filemode='w', force=True,
@@ -115,7 +116,6 @@ df_values = {'exr': None, 'cp': None, 'pr': None}  # To be used for subset of va
 # Define download (extract) and load location - In this example, it assumes that the
 #   default (code) directory is 'src', and that a 'data' directory exists in parallel;
 #   this can be configured to anything
-ds_path = '../data/'
 
 # Iterate dataset
 for dataset in ds:
@@ -123,15 +123,15 @@ for dataset in ds:
     print('Processing '+ds[dataset]['efname'], end='... ')
 
     # Download and extract file; if error, log and skip to next file in dataset
-    if not download_and_extract_data(url + ds[dataset]['efname'], ds[dataset]['efname'], ds_path):
+    if not download_and_extract_data(url + ds[dataset]['efname'], ds[dataset]['efname'], _data_path):
         continue
 
     # Load dataframe
     df[dataset], df_ids[dataset], df_values[dataset] = load_data(ds[dataset]['lfname'],
-                                                                 ds_path, ds[dataset]['id_vars'])
+                                                                 _data_path, ds[dataset]['id_vars'])
 
     # Confirm control counts
-    ctrl = ctrl_count(ds_path+ds[dataset]['lfname'])
+    ctrl = ctrl_count(_data_path+ds[dataset]['lfname'])
     pipeline_log.info('{0}_file={1}_import={2}_delta={3}'. \
                       format(ds[dataset]['lfname'], ctrl, len(df[dataset]), ctrl-len(df[dataset])))
 
@@ -146,7 +146,7 @@ for dataset in ds:
 # Data Transformation
 # **********************************************************************
 
-### Renaming key columns to be imported into the schema to make them more workable within MySQL
+# Renaming key columns to be imported into the schema to make them more workable within MySQL
 df_ids['exr'] = df_ids['exr'].copy() # added to avoid SettingWithCopyWarning
 df_ids['exr'].rename(columns = {'Frequency': 'frequency', 'Reference area': 'reference_area', 'Currency': 'currency', 
                                 'Collection': 'collection', 'Unit Multiplier': 'unit_multiplier', 'DECIMALS': 'decimals',
@@ -164,7 +164,7 @@ df_values['exr'].rename(columns = {'index': 'exchange_rate_id', 'variable': 'dat
 df_values['pr'].rename(columns = {'index': 'policy_rate_id', 'variable': 'date'}, inplace = True)
 df_values['cp'].rename(columns = {'index': 'consumer_prices_id', 'variable': 'date'}, inplace = True)
 
-### Creating connection to local machine // formulas to nest df to 
+# Creating connection to local machine // formulas to nest df to
 def run_connection(db_connection: pymysql, syntax: str) -> None:
     """
     Run Syntax.
@@ -176,7 +176,7 @@ def run_connection(db_connection: pymysql, syntax: str) -> None:
     cur.execute(syntax)
     cur.close()
 
-### Can create a table if need be within notebook
+# Can create a table if need be within notebook
 def create_table(schema: str, table: str) -> None:
     """
     Create a new table in the connected database on the schema
@@ -196,7 +196,7 @@ def create_table(schema: str, table: str) -> None:
     db_conn.commit()
     db_conn.close()
 
-### populate the table with new records
+# populate the table with new records
 def populate_table(table_name: str, df: pd.DataFrame) -> None:
     """ 
     Insert df into table within database
@@ -218,23 +218,23 @@ def populate_table(table_name: str, df: pd.DataFrame) -> None:
         db=_db,
     )
 
-    ### connecting to cursor to pull column names in table
+    # connecting to cursor to pull column names in table
     cur = db_conn.cursor()
     cur.execute(f"SELECT * FROM {table_name} LIMIT 0")
     cur.close()
 
-    ### obtaining table column names
+    # obtaining table column names
     col_names = [i[0] for i in cur.description]
 
-    ### Ensure that updates do not duplicate by syncing table indexes and dataframe indexes
+    # Ensure that updates do not duplicate by syncing table indexes and dataframe indexes
     index1 = pd.read_sql(f"SELECT {table_name}_id FROM {table_name};", db_conn).index
     index2 = df.index
     indiff = index2.difference(index1)
 
-    ### Extracting indexes not seen in table on database
+    # Extracting indexes not seen in table on database
     df = df.loc[indiff]
 
-    ### Setting null value to be set auto incrementally
+    # Setting null value to be set auto incrementally
     df[f"{table_name}_id"] = np.NaN * len(df.index)
 
     ### Identify missing columns
@@ -245,7 +245,7 @@ def populate_table(table_name: str, df: pd.DataFrame) -> None:
     if len(missing_columns) > 0:
         pipeline_log.error('Missing Columns Critical Error')
 
-    ### extracting needed columns from dataframes
+    # extracting needed columns from dataframes
     df = df[col_names]
 
     df.to_sql(table_name, engine, if_exists = 'append', index = False)
@@ -255,7 +255,7 @@ def populate_table(table_name: str, df: pd.DataFrame) -> None:
 
     warnings.resetwarnings()
 
-### Table names from Schema for ID
+# Table names from Schema for ID
 table_names = ['exchange_rate','consumer_prices','policy_rate']
 value_table_names = ['exchange_rate_values','consumer_prices_values','policy_rate_values']
 
@@ -278,7 +278,7 @@ for i in zip(df_values, value_table_names):
 
 
 
-## Establish CPI and Fed Rate Dataframes
+# Establish CPI and Fed Rate Dataframes
 
 # Create connection to the MySQL server
 conn = pymysql.connect(host=_host, user=_user, passwd=_passwd, db=_db)
@@ -315,8 +315,8 @@ conn.close()
 # Reset warnings to default
 warnings.resetwarnings()
 
-##
-## Perform Basic Time Series Analysis - CPI and Fed Rate
+#
+# Perform Basic Time Series Analysis - CPI and Fed Rate
 
 print('Plotting CPI_v_Fedrate', end='... ')
 
@@ -336,7 +336,7 @@ ax.axvline(x=cpi_df['cpv_date'].max(), color='lightgray', linestyle='dashed', lw
 plt.legend()
 
 # Show plot and save it for e.g., dashboard or Web
-#plt.show()
+# plt.show()
 fig.savefig('CPI_v_FedRate.svg', format='svg')
 
 pipeline_log.info('CPI_v_FedRate generated')
